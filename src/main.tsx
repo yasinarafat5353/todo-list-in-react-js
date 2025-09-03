f53642bc825a6668b8da9611cabab86f70448ec6
yasinarafat5353 <yasinarafat.cid.bd@gmail.com>
2025-09-03 17:31:32 +0600

    class 51

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import type { TodoStateType } from './store/todo/todoInterface'
+import { TodoContext } from './store/todo/todoContext'
+
+const todos: TodoStateType = [
+        {
+            id: 1,
+            title: "this is title from context",
+            status: "Done"
+        }
+    ]
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TodoContext value={todos}>
+        <App />
+         </TodoContext>
+      </StrictMode>,
+   
+)    
